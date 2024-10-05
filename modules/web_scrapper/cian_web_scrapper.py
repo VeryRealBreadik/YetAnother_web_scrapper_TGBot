017f6dd63f9feef92fa800e017312cbfe9edaf85
XJIE6yIIIEK <aleksey_stankovskiy@rambler.ru>
2024-10-06 00:35:54 +0300

    modules for bot are done; bot module, main.py, module initialization file left to code

--- modules/web_scrapper/cian_web_scrapper.py
+++ modules/web_scrapper/cian_web_scrapper.py
@@ -6,20 +6,9 @@ import random
 
 
 class WebScrapper:
-    def __init__(self):
+    def __init__(self, headers):
         self.offers_urls = []
-        user_agent = UserAgent()
-        self.headers = {
-            "User-Agent" : user_agent.random,
-            "Accept-Language" : "ru,en;q=0.9",
-            "Accept-Encoding" : "gzip, deflate, br, zstd",
-            "Accept" : "*/*",
-            "Referer" : "https://ya.ru/",
-            "Sec-Fetch-Dest" : "empty",
-            "Sec-Fetch-Mode" : "navigate",
-            "Sec-Fetch-Site" : "none",
-            "Cookie" : "is_gdpr=0; i=41b/RB2b5LGOpEA0x4afsq0Hpf573OGppI4gVlx8hrwW0m+DNyuncSdgW0q2mBV4Z1TjSRhuM0+Q9oZpNF4TDE5AhfA=; yandexuid=3038451981728142174; yashr=4119276311728142174; is_gdpr_b=CNesfBD6lgIoAg==; receive-cookie-deprecation=1; bh=Ek8iTm90L0EpQnJhbmQiO3Y9IjgiLCAiQ2hyb21pdW0iO3Y9IjEyNiIsICJZYUJyb3dzZXIiO3Y9IjI0LjciLCAiWW93c2VyIjt2PSIyLjUiGgIiIiINIjI0LjcuNC4xMzE2IioCPzEyCSJOZXh1cyA1IjoJIkFuZHJvaWQiQgUiNi4wIkoEIjY0IlJnIk5vdC9BKUJyYW5kIjt2PSI4LjAuMC4wIiwgIkNocm9taXVtIjt2PSIxMjYuMC42NDc4LjIzNCIsICJZYUJyb3dzZXIiO3Y9IjI0LjcuNC4xMzE2IiwgIllvd3NlciI7dj0iMi41IloCPzBggLiFuAZqI9zKpewGz5+MnwWsp7y7BaCd7OsD/Lmv/wff/ffHAeW1zYcI; my=YwA=; KIykI=1; _yasc=14/cm2pvToHFeSUik7WZpG77yKUcr5cKSMXhbbSjSpMBXf5JJjD3WzYca7PwVGtVUuh6RNjSOVFN; yp=1728212963.uc.ru#1728212963.duc.gt#1759678153.cld.2270452#1759678153.brd.0702004923#2043502343.pcs.0#1730219947.hdrc.1#1729267607.hgclkp.2272#1742110375.szm.2:877x400:400x877#1730819195.csc.1#2041941376.udn.cDrQkNC70LXQutGB0LXQuQ%3D%3D#1759678343.swntab.0#1728149364.gpauto.55_755863%3A37_617699%3A100000%3A3%3A1728142164#1728927547.dlp.3#1743910338.sz.877x400x2; ys=wprid.1728142363409690-11891047049706500503-balancer-l7leveler-kubr-yp-klg-308-BAL",
-        }
+        self.headers = headers
 
     def scrape_cian(self, url):
         count = 0
@@ -34,8 +23,6 @@ class WebScrapper:
             sleep_value = random.randint(1, 3) / 10 * 6
             time.sleep(sleep_value)
             count += 1
-
-        print(f"Ссылки успешно выгружены! Число выгруженных ссылок: {len(self.offers_urls)}")
 
     def scrape_offer(self, offer_url):
         offer_info_dict = {}
